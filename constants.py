310a3781b120eff6886ca65a4d6101af5a344116
Cargill Seiveright <microcargill@hotmail.com>
2020-11-28 18:26:30 -0500

    Add basic structure

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""Constants for I/O System."""
+
+WINDOW_RESOLUTION = (1351, 900)
+WINDOW_NAME = "I/O System Game"
+
+"""Background Image."""
+BACKGROUND_IMAGE = './images/os-game.png'
+BACKGROUND_POSITION = (0, 0)
+BACKGROUND_COLOR = (255, 255, 255)
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+"""I/O System Game"""
 import pygame
+import constants
+
 
 # Intialize the pygame
 pygame.init()
 
 # create the screen
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode(constants.WINDOW_RESOLUTION)
+
+# Caption and Icon
+pygame.display.set_caption(constants.WINDOW_NAME)
+# icon = pygame.image.load('ufo.png')
+# pygame.display.set_icon(icon)
 
 running = True
 
 while  running:
-    # RGB = Red, Green, Blue
-    screen.fill((0, 0, 0))
-    
+    screen.fill(constants.BACKGROUND_COLOR)
+
+    background = pygame.image.load(constants.BACKGROUND_IMAGE)
+    screen.blit(background, constants.BACKGROUND_POSITION)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    pygame.display.update()
